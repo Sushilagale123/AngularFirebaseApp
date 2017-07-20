4b3953b14e68e90c4716a1113b87a4ec1bca791a
Sushilagale123 <sushil.agale@gmail.com>
2017-07-20 11:56:40 +0530

    AngularFirebaseApp

--- AngularFirebaseApp/formScript.js
+++
@@ -1,8 +0,0 @@
-
-app.controller("formCtrl", function($scope){
-    $scope.firstName = "Sushil"
-    $scope.lastName = "Agale"
-    $scope.contact = 9503292492
-    $scope.email = "sushil@gmail.com"
-    
-})
--- AngularFirebaseApp/jsonCtrl.js
+++
@@ -1,8 +0,0 @@
-app.controller("jsonCtrl", function($scope, $http){
-    
-    $http.get('data.json').then(function(response){
-        
-        $scope.content = response.data.student
-    })
-    
-})
